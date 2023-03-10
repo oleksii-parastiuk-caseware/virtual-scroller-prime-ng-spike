b1a16b818f91d446abeb25ecf81d26f8dea66f6f
oparasti <oleksii.parastiuk@caseware.com>
2023-03-10 15:59:07 +0200

    add spike results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { Account, AccountService } from './account.service';
+import { VirtualScroller } from 'primeng/virtualscroller';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'prime-ng-testing';
+  @ViewChild('vs') vs: VirtualScroller = null as any;
+
+  filteredAccounts$ = new BehaviorSubject<Account[]>([]);
+  draggedAccounts$ = new BehaviorSubject<Account[]>([]);
+
+  availableAccounts: Account[] = [];
+  checkedAccounts: Account[] = [];
+  selectedAccounts: Account[] = [];
+
+  query$ = new BehaviorSubject<string>('');
+
+  constructor(private accountService: AccountService) {
+    this.accountService.generateAccounts$().subscribe((accounts) => {
+      this.availableAccounts = accounts;
+      this.filteredAccounts$.next(accounts);
+    });
+  }
+
+  /* @HostListener('dragstart', ['$event'])
+  async onDragStart(event: DragEvent) {
+    const img = new Image();
+    img.src = '../assets/penguin.png';
+    event.dataTransfer?.setDragImage(img, 250, 250);
+  } */
+
+  dragStart(event: Account): void {
+    const draggedAccounts = this.checkedAccounts.length
+      ? this.checkedAccounts
+      : [event];
+
+    this.draggedAccounts$.next(draggedAccounts);
+  }
+
+  dragEnd(): void {
+    this.draggedAccounts$.next([]);
+    this.checkedAccounts = [];
+  }
+
+  check(checked: boolean, { id }: Account): void {
+    const account = this.availableAccounts.find((account) => account.id === id);
+
+    if (!account) return;
+
+    if (checked) {
+      this.checkedAccounts.push(account);
+    }
+
+    if (!checked) {
+      const updatedSelection = this.checkedAccounts.filter(
+        (checkedAccount) => checkedAccount.id !== account.id
+      );
+
+      this.checkedAccounts = updatedSelection;
+    }
+  }
+
+  drop(): void {
+    const draggedAccounts = this.draggedAccounts$.value;
+
+    if (!draggedAccounts.length) {
+      return;
+    }
+
+    const draggedAccountsIds = this.draggedAccounts$.value.map(({ id }) => id);
+
+    this.selectedAccounts = [
+      ...this.selectedAccounts,
+      ...this.draggedAccounts$.value,
+    ];
+
+    const updated = this.filteredAccounts$.value.reduce((acc, account) => {
+      const match = draggedAccountsIds.some((id) => id === account.id);
+
+      if (!match) acc.push(account);
+
+      return acc;
+    }, [] as Account[]);
+
+    this.filteredAccounts$.next(updated);
+    this.checkedAccounts = [];
+    this.draggedAccounts$.next([]);
+  }
+
+  scrollToTop(): void {}
+
+  searchAccount(query: string) {
+    if (query.length < 3) {
+      if (this.allAccountsAreDisplayed) return;
+
+      this.filteredAccounts$.next(this.availableAccounts);
+    }
+
+    this.vs.scrollToIndex(0);
+
+    const result = this.availableAccounts.filter((account) =>
+      account.name.includes(query)
+    );
+
+    this.filteredAccounts$.next(result);
+  }
+
+  private get allAccountsAreDisplayed(): boolean {
+    return (
+      this.filteredAccounts$.value.length === this.availableAccounts.length
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DragDropModule } from 'primeng/dragdrop';
+import { InputTextModule } from 'primeng/inputtext';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    VirtualScrollerModule,
+    CheckboxModule,
+    DragDropModule,
+    TableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
